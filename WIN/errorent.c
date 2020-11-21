0e7564473f7b361136750d137186ae544f0b0477
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-11-21 20:28:14 +0100

    Missing_windows_sources

---
+++ WIN/errorent.c
@@ -0,0 +1,29 @@
+/******************************************************************************************
+*Setting this variable configure SYMSHELL
+*for forcing stop before starting cleanup on exit 
+*******************************************************************************************/
+#include "SYMSHELL/symshell.h"		// prototypes of symshell graphix primitives
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int WB_error_enter_before_clean=1;
+
+#ifdef  __cplusplus
+}
+#endif
+
+
+/********************************************************************/
+/*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
+/*            W O J C I E C H   B O R K O W S K I                   */
+/* Zaklad Systematyki i Geografii Roslin Uniwersytetu Warszawskiego */
+/*  & Instytut Studiow Spolecznych Uniwersytetu Warszawskiego       */
+/*        WWW:  http://moderato.iss.uw.edu.pl/~borkowsk             */
+/*        MAIL: borkowsk@iss.uw.edu.pl                              */
+/*                               (Don't change or remove this note) */
+/********************************************************************/
+
+
+
---
+++ WIN/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Symshmsc.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

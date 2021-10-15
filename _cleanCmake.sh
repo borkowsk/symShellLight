0b32772bc59eda7b464c5bc950ac18068ea8aad9
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-10-15 14:09:32 +0200

    Wkopiowano z innego projektu

---
+++ _cleanCmake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -ne 0 ]; 
+then
+   echo "No parameters expected!" 1>&2
+   exit -1
+fi
+
+echo "Cleaning cmake files!"
+echo "Cleaning current directoryfrom cmake files!" `date` >> clean_list.txt
+
+rm -f CMakeCache.txt
+rm -f Makefile
+rm -f cmake_install.cmake
+rm -Rf CMakeFiles

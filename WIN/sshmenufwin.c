73d2b2c4234e9fd654302c81266799af1b0a66ef
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-12-30 15:45:27 +0100

    Brakujące_funkcje_z_windows

--- WIN/sshmenufwin.c
+++ WIN/sshmenufwin.c
@@ -1,5 +1,5 @@
 /* IMPLEMENTATION FOR SYMSHELL MENU AND RELEATED FEATURES */
-/** \date  2022 - 12 - 23 (last modification)             */
+/** \date  2022 - 12 - 30 (last modification)             */
 /**********************************************************/
 //#include "platform.h"
 
@@ -28,8 +28,8 @@ extern HWND	WB_Hwnd;//W symshwin.c
 /// \note Wygl¹da ¿e sta³o siê redundantne z nowsz¹ funkcj¹ set_title()
 int ssh_set_window_name(const char* WindowName)
 {
-	//NOT IMPLEMENTED!!!
-	return 0;
+	return SetWindowText(WB_Hwnd, WindowName);
+	// SetWindowText return TRUE on success!
 }
 
 ssh_menu_handle ssh_main_menu()
@@ -40,15 +40,14 @@ ssh_menu_handle ssh_main_menu()
 ssh_menu_handle ssh_sub_menu(
 					ssh_menu_handle hMenu,				
 					unsigned    Position)
-{
+{										assert(hMenu != 0);
 	return GetSubMenu((HMENU)hMenu,Position);
 }
 
 unsigned ssh_get_item_position(
 					 ssh_menu_handle hMenu,
 					 const char* ItemName)
-{
-																		assert(hMenu != 0);
+{										assert(hMenu != 0);
 	size_t i,len=strlen(ItemName);
 	char* pom=malloc(len+1);
 	int N=GetMenuItemCount(hMenu);
@@ -56,7 +55,7 @@ unsigned ssh_get_item_position(
 	for(i=0;i<N;i++)
 	{
 		int ret=GetMenuString(hMenu,i,pom,len+1,MF_BYPOSITION);
-																        assert(ret!=0);
+									        assert(ret!=0);
 		if(strcmp(ItemName,pom)==0)
 		{
 			free(pom);
@@ -69,20 +68,20 @@ unsigned ssh_get_item_position(
 }
 
 int	ssh_menu_add_item(
-					ssh_menu_handle hMenu,
-					const char* ItemName,
-					unsigned    Message,
-					unsigned    Flags)
-{
+				ssh_menu_handle hMenu,
+				const char* ItemName,
+				unsigned    Message,
+				unsigned    Flags)
+{										assert(hMenu != 0);
 	/*wchar_t	UniItemName[1024];
 	//I tak nie dziala - zostaja krzaczki
 	int ret=MultiByteToWideChar(
-		1252,//CP_ACP,			// UINT CodePage,         // code page
-		MB_PRECOMPOSED	,		//DWORD dwFlags,         // character-type options
-		ItemName,				//LPCSTR lpMultiByteStr, // address of string to map
-	  strlen(ItemName)+1,		//number of bytes in string
-		UniItemName,			//LPWSTR lpWideCharStr,  // address of wide-character buffer
-		1024					//int cchWideChar        // size of buffer
+		1252,//CP_ACP,		       // UINT CodePage,         // code page
+		MB_PRECOMPOSED	,	       // DWORD dwFlags,         // character-type options
+		ItemName,		       // LPCSTR lpMultiByteStr, // address of string to map
+	  strlen(ItemName)+1,		       // number of bytes in string
+		UniItemName,			// LPWSTR lpWideCharStr,  // address of wide-character buffer
+		1024				// int cchWideChar        // size of buffer
 	);
 	 */
 	if(Flags==0)/* Ma byc domyslnie */
@@ -97,21 +96,25 @@ int	ssh_menu_add_item(
 /// \param asPosition
 /// \return ???
 int ssh_menu_remove_item(
-	ssh_menu_handle hMenu,
-	unsigned ItemCommandOrPosition,
-	unsigned asPosition)
-{
-	//NOT IMPLEMENTED!!!
-	return 0;
+				ssh_menu_handle hMenu,
+				unsigned ItemCommandOrPosition,
+				unsigned asPosition)
+{										assert(hMenu != 0);
+	UINT Flags = 0;
+	if (asPosition)
+		Flags |= MF_BYPOSITION;
+	else
+		Flags |= MF_BYCOMMAND;
+	return RemoveMenu(hMenu, ItemCommandOrPosition, Flags) != 0xffffffff;
 }
 
 int ssh_menu_mark_item(
-					ssh_menu_handle hMenu,
-					unsigned    Check,
-					unsigned    ItemCommandOrPosition,										
-					unsigned    asPosition
-					   )
-{
+				ssh_menu_handle hMenu,
+				unsigned    Check,
+				unsigned    ItemCommandOrPosition,										
+				unsigned    asPosition
+			)
+{										assert(hMenu != 0);
 	UINT Flags=0;
 	if(Check) Flags|=MF_CHECKED;
 		else  Flags|=MF_UNCHECKED;
@@ -120,8 +123,27 @@ int ssh_menu_mark_item(
 	return  CheckMenuItem(hMenu,ItemCommandOrPosition,Flags)!=0xffffffff;
 }
 
+/// Ustawia lub usuwa marker przy itemie
+int ssh_menu_mark_item2(	ssh_menu_handle hMenu,
+				unsigned Check,
+				unsigned ItemCommandOrPosition,
+				unsigned asPosition
+			)
+{										assert(hMenu != 0);
+	UINT Flags = 0;
+	if (Check)
+		Flags |= MF_CHECKED;
+	else
+		Flags |= MF_UNCHECKED;
+	if (asPosition)
+		Flags |= MF_BYPOSITION;
+	else
+		Flags |= MF_BYCOMMAND;
+	return CheckMenuItem(hMenu, ItemCommandOrPosition, Flags) != 0xffffffff;
+}
+
 int ssh_realize_menu(ssh_menu_handle hMenu)
-{
+{										assert(hMenu != 0);
 	return DrawMenuBar(WB_Hwnd ); 
 }
 

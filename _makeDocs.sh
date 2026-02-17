ac1c1ce057fb179a4e00ad487555cf99c1693c20
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-02-17 12:44:24 +0100

    Nowe pseudologo w dokumentacji

--- _makeDocs.sh
+++ _makeDocs.sh
@@ -2,13 +2,6 @@
 (whereis doxygen | grep "/doxygen") && echo "OK, doxygen installed"
 (whereis doxywizard | grep "/doxywizard") && echo "OK, doxywizard installed"
 
-if ! test -d "DOCS/"
-then 
-    mkdir DOCS/
-else
-    echo "DOCS/ directory already exist"
-fi 
-
 echo;echo "RUNNING Doxygen:" ;echo
 
 #run Doxygen directly

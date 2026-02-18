2bbef7f76b3d143e45a3501673b5bb3d21e1f38d
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-02-18 17:39:14 +0100

    Zmiana formatki koncowej

--- UTILS/sshdefs.c
+++ UTILS/sshdefs.c
@@ -6,14 +6,14 @@ char*        	ssh_menu_options=""; //"-fg black -bg gray";?
 /// For debugging purposes - externally accessible
 int 			ssh_menu_trace=0;
 
-/********************************************************************/
-/*              SYMSHELLLIGHT  version 2021-11-24                   */
-/********************************************************************/
-/*           THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
-/*            W O J C I E C H   B O R K O W S K I                   */
-/*    Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
-/*    WWW: https://www.researchgate.net/profile/WOJCIECH_BORKOWSKI  */
-/*    GITHUB: https://github.com/borkowsk                           */
-/*                                                                  */
-/*                               (Don't change or remove this note) */
-/********************************************************************/
+/* *******************************************************************/
+/*                   SYMSHELLLIGHT  version 2026                     */
+/* *******************************************************************/
+/*            THIS CODE IS DESIGNED & COPYRIGHT  BY:                 */
+/*             W O J C I E C H   B O R K O W S K I                   */
+/*     Instytut Studiów Społecznych Uniwersytetu Warszawskiego       */
+/*     WWW: https://www.researchgate.net/profile/WOJCIECH_BORKOWSKI  */
+/*     GITHUB: https://github.com/borkowsk                           */
+/*                                                                   */
+/*                                (Don't change or remove this note) */
+/* *******************************************************************/

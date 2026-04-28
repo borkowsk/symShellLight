5199c5172a9ae2b050cfe2d94e61da55f60a3996
Wojciech T. Borkowski <wborkowski@uw.edu.pl>
2026-04-28 16:40:22 +0200

    Skrypt aktualizacji tego repo jako submodułu  jakiegoś innego.

---
+++ _update_as_submodule.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# @date 2026-04-28 (last modification)
+# ==============================
+#Ten skrypt aktualizuje SymShellLight zainstalowany jako submodul innego repo.
+
+cd ..
+git submodule update --remote --merge --recursive
+git add .
+git commit -m "Aktualizacja submodułu do najnowszej wersji"
+git push
